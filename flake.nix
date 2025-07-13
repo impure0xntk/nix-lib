506c6df5af58a1eca1e8a3628a19f514c939c426
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-07-13 12:25:24 +0900

    fix(flake): output

--- flake.nix
+++ flake.nix
@@ -7,23 +7,21 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     let
       overlay = final: prev: {
-        lib = prev.lib.extend (selflib: superlib: {
-          my = import ./lib {
-            pkgs = final;
-            lib = superlib;
-          };
-        });
+        my = import ./lib {
+          pkgs = final;
+          lib = prev;
+        };
       };
     in
     {
       overlays.default = overlay;
     } // flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
-        lib = pkgs.lib;
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib.extend (self.overlays.default);
       in
       {
-        lib = lib.my;
+        inherit lib;
         checks.lib-extend = import ./tests { inherit pkgs lib; };
       });
 }
