3221a72e6596128f3eb5e47d90417b3316e3edaa
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-07-13 15:39:08 +0900

    fix(flake): missing of pkgs

--- flake.nix
+++ flake.nix
@@ -4,24 +4,32 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    let
-      overlay = final: prev: {
-        my = import ./lib {
-          pkgs = final;
-          lib = prev;
-        };
-      };
-    in
+  outputs =
     {
-      overlays.default = overlay;
-    } // flake-utils.lib.eachDefaultSystem (system:
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      createLibOverlay =
+        pkgs:
+        (final: prev: {
+          my = import ./lib {
+            inherit pkgs;
+            lib = prev;
+          };
+        });
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        lib = pkgs.lib.extend (self.overlays.default);
+        lib = pkgs.lib.extend (createLibOverlay pkgs);
       in
       {
         inherit lib;
         checks.lib-extend = import ./tests { inherit pkgs lib; };
-      });
+      }
+    );
 }
--- tests/default.nix
+++ tests/default.nix
@@ -6,5 +6,8 @@ pkgs.runCommand "overlay-test" { } ''
   echo "${lib.strings.concatStringsSep " " (lib.my.listDirs {path = ./..;})}" > $out
   SIZE=$(stat -c %s $out)
   test "$SIZE" -gt 0
+
+  # builders
+  echo ${lib.my.writeOilApplication { name = "test"; text = "true";}}
   set +x
 ''
